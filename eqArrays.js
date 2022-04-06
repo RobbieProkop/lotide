0ebc6e549b9943b9925c3e8a9879c9995da2ed5c
Robbie Prokop <robbieprokop@gmail.com>
2022-04-06 03:02:19 +0000

    created without function to remove elements of arrays, changed array1/2 to actual/expected in eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -6,29 +6,26 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function (array1, array2) {
+const eqArrays = function (actual, expected) {
   // USING NOT EQUALS
-  if (array1.length !== array2.length) return false;
-
-  let isEqual = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
+  if (actual.length !== expected.length) return false;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) return false;
   }
   return true;
-
-  // USING EQUALS
-  // if (array1.length === array2.length) {
-  //   let isEqual = true;
-  //   for (let i = 0; i < array1.length; i++) {
-  //     if (array1[i] === array2[i]) {
-  //       // console.log(isEqual);
-  //     } else {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // }
 };
+// USING EQUALS
+// if (array1.length === array2.length) {
+//   let isEqual = true;
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] === array2[i]) {
+//       // console.log(isEqual);
+//     } else {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
 // console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 console.log(eqArrays([1, 2, 3], [3, 2, 1]));
---
+++ without.js
@@ -0,0 +1,51 @@
+const eqArrays = function (actual, expected) {
+  // USING NOT EQUALS
+  if (actual.length !== expected.length) return false;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  // USING NOT EQUALS
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+// need to take in an original array (actual), and itemsToRemove array (expected)
+
+const without = function (actual, expected) {
+  //  should retrun a new array
+  let newArr = [];
+
+  //  should return only elements from the original array that have not been removed (not present in toRemove)
+
+  // first we need to see if there are any matches between the two arrays
+  // if the element does not match, push it to the new array
+  // if it does match, do nothing
+  if (!eqArrays(actual, expected)) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        newArr.push(actual[i]);
+        // console.log(newArr);
+      }
+    }
+  }
+  return newArr;
+};
+
+// console.log(without([1, 2, 3], [1])); // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+assertArraysEqual(without(["1", "2", "3", "88"], [1, 2, "3", "4", 6, 4, 3]), [
+  "1",
+  "2",
+]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3", "4", 6, 4, 3]), [
+  "1",
+  "2",
+]);
