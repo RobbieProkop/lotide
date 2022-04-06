6da4b4c4ce5b899d16ba4239795694bbdb30636b
Robbie Prokop <robbieprokop@gmail.com>
2022-04-06 02:31:39 +0000

    added more cases to check

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,3 +16,7 @@ const assertArraysEqual = function (actual, expected) {
   }
 };
 assertArraysEqual([1, 2, 3], [3, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [3, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
