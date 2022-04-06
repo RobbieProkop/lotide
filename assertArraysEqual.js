4ce18a20d88984dddccd6c34d4ca56765c8fe55e
Robbie Prokop <robbieprokop@gmail.com>
2022-04-06 02:16:57 +0000

    assertArraysEqual.js function

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = function (array1, array2) {
+  // USING NOT EQUALS
+  if (array1.length !== array2.length)
+    return `⛔️⛔️⛔️Assertion Failed: ${array1} !==  ${array2}`;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return `⛔️⛔️⛔️Assertion Failed: ${array1} !==  ${array2}`;
+  }
+  return `✅✅✅Assertion Passed: ${array1} ===  ${array2}`;
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
