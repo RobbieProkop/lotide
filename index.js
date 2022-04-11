0b8f021ca7192ab5a09f5e1ce53d7e014df308ea
Robbie Prokop <robbieprokop@gmail.com>
2022-04-11 23:51:30 +0000

    lots of changes exporting functions to an index.js file

---
+++ index.js
@@ -0,0 +1,31 @@
+const assertArraysEqual = require("./lib/assertArraysEqual");
+const assertObjectsEqual = require("./lib/assertObjectsEqual");
+const assertEqual = require("./lib/assertEqual");
+const countLetters = require("./lib/countLetters");
+const eqArrays = require("./lib/eqArrays");
+const findKey = require("./lib/findKey");
+const findKeyByValue = require("./lib/findKeyByValue");
+const head = require("./lib/head");
+const letterPositions = require("./lib/letterPositions");
+const map = require("./lib/map");
+const middle = require("./lib/middle");
+const tail = require("./lib/tail");
+const takeUntil = require("./lib/takeUntil");
+const without = require("./lib/without");
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  assertEqual: assertEqual,
+  countLetters: countLetters,
+  eqArrays: eqArrays,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail: tail,
+  takeUntil: takeUntil,
+  without: without,
+};
