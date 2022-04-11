b6b9b247db74d970af8592dc25aa0e8b70dffce0
Robbie Prokop <robbieprokop@gmail.com>
2022-04-11 22:44:04 +0000

    removed commented tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,3 @@
-// const middle = require("../lib/middle");
-// const assertArraysEqual = require("../lib/assertArraysEqual");
-
-// assertArraysEqual(middle([1]), []); // => []
-// assertArraysEqual(middle([1, 2]), []); // => []
-// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-
 const assert = require("chai").assert;
 const middle = require("../lib/middle");
 
