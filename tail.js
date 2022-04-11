b0cc3fc36ee2826808f5cb2f04c0174289c9a290
Robbie Prokop <robbieprokop@gmail.com>
2022-04-11 13:39:36 +0000

    refactored tail to expert and import functions. created tailTest.js

--- tail.js
+++ tail.js
@@ -1,19 +1,14 @@
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`⛔️⛔️⛔️Assertion Failed: ${actual} !==  ${expected}`);
-  } else if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} ===  ${expected}`);
-  }
-};
-
+const assertEqual = require("./assertEqual");
 let tail = function (array) {
-  let newArray = array.slice(1);
-  return newArray;
+  let newArray = array.slice(1); // cuts off the first element
+  return newArray; // returns the tail of the array (everything but the first)
 };
-let result = tail(["head", "hello", "world", "I'm Robbie"]);
+// let result = tail(["head", "hello", "world", "I'm Robbie"]);
 // console.log(result[2]);
 
 // assertEqual("hello", "hello"); // comparing strings
 // assertEqual(["hello", "1"].length, ["hello", "3"].length); // comparing the amount of elements
 // assertEqual(words[3], "I'm Robbie"); // comparing the specific element to a string
-assertEqual(result.length, 3);
+// assertEqual(result.length, 3);
+
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
+let result = tail(["head", "hello", "world", "I'm Robbie"]);
+// console.log(result);
+// assertEqual("hello", "hello"); // comparing strings
+// assertEqual(["hello", "1"].length, ["hello", "3"].length); // comparing the amount of elements
+assertEqual(result[2], "I'm Robbie"); // comparing the specific element to a string
+// assertEqual(result.length, 3);
